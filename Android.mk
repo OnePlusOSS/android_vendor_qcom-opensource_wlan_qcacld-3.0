410334843fb29025eb91fda5684dce53a5d1305a
Jeff Chen <jeff.chen@oneplus.net>
2017-09-30 22:23:57 +0800

    [DeviceTree][8998O][WiFi] Fix create soft link command error
    Change-Id: I5c88b2c45e022c21d9095249c1750364ab0683a1

--- Android.mk
+++ Android.mk
@@ -78,9 +78,11 @@ endif
 endif
 
 ifeq ($(PRODUCT_VENDOR_MOVE_ENABLED),true)
-$(shell ln -sf /persist/wlan_mac.bin $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/wlan_mac.bin)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/; \
+		ln -sf /persist/wlan_mac.bin $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/wlan_mac.bin)
 else
-$(shell ln -sf /persist/wlan_mac.bin $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin)
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/; \
+		ln -sf /persist/wlan_mac.bin $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin)
 endif
 
 endif # DLKM check
